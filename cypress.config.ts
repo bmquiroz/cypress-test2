6637d75c04c9665421c382e03c04ce390aa515c3
Boris Quiroz <bmquiroz@outlook.com>
2023-01-08 11:55:41 -0500

    ...

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: "qw7cqy",
   fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:8888',
   },
+
 })
